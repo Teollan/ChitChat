87142b9878c1dc5c654a20b784e83b470942836f
Serhii Kozlov <serhiikofficial@gmail.com>
2024-12-29 15:11:35 +0200

    backend setup

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { ignores: ['node_modules', 'dist/*'] },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    semi: true,
+                    singleQuote: true,
+                    tabWidth: 4,
+                    trailingComma: 'es5',
+                    printWidth: 120,
+                    arrowParens: 'always',
+                },
+            ],
+            'max-len': [
+                'warn',
+                {
+                    code: 120,
+                    ignoreComments: true,
+                    ignoreUrls: true,
+                },
+            ],
+            '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+        },
+    },
+];
